3683b57fdc0e36508972008a21aa120eef106dcb
luckuveryx <luckuveryx@icloud.com>
2023-10-14 09:02:59 +0800

    chore: bloc

--- test/app/view/app_test.dart
+++ test/app/view/app_test.dart
@@ -1,12 +1,12 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:luckuveryx/app/app.dart';
-import 'package:luckuveryx/counter/counter.dart';
 
 void main() {
   group('App', () {
-    testWidgets('renders CounterPage', (tester) async {
+    testWidgets('renders MaterialApp', (tester) async {
       await tester.pumpWidget(const App());
-      expect(find.byType(CounterPage), findsOneWidget);
+      expect(find.byType(MaterialApp), findsOneWidget);
     });
   });
 }
