3da4b604086f79c1f48d1363b7498a9a62f04c63
luckuveryx <luckuveryx@icloud.com>
2023-10-14 09:49:55 +0800

    fix: missing provider scope in tests

--- test/app/view/app_test.dart
+++ test/app/view/app_test.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:luckuveryx/app/app.dart';
 
 void main() {
   group('App', () {
     testWidgets('renders MaterialApp', (tester) async {
-      await tester.pumpWidget(const App());
+      await tester.pumpWidget(const ProviderScope(child: App()));
       expect(find.byType(MaterialApp), findsOneWidget);
     });
   });
