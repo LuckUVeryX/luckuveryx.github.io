eb9eb6be8267e7b1be60940cf7e79fef882e581d
luckuveryx <luckuveryx@icloud.com>
2023-10-14 19:20:05 +0800

    feat: animated widgets

---
+++ lib/widgets/molecules/animated_bounce.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+
+double _useBounce({
+  required Duration duration,
+}) {
+  final controller = useAnimationController(duration: duration);
+  useEffect(
+    () {
+      controller
+        ..addStatusListener((status) {
+          final onStatusChanged = switch (status) {
+            AnimationStatus.completed => controller.reverse,
+            AnimationStatus.dismissed => controller.forward,
+            _ => null,
+          };
+          onStatusChanged?.call();
+        })
+        ..forward();
+      return null;
+    },
+    const [],
+  );
+  return useAnimation(
+    CurvedAnimation(
+      parent: controller,
+      curve: Curves.easeOutCubic,
+    ),
+  );
+}
+
+class AnimatedBounce extends HookWidget {
+  const AnimatedBounce({
+    required this.child,
+    this.offset = const Offset(4, 0),
+    this.duration = const Duration(milliseconds: 500),
+    super.key,
+  });
+
+  final Widget child;
+  final Duration duration;
+  final Offset offset;
+
+  @override
+  Widget build(BuildContext context) {
+    final bounce = _useBounce(duration: duration);
+    return Transform.translate(
+      offset: offset * bounce,
+      child: child,
+    );
+  }
+}
