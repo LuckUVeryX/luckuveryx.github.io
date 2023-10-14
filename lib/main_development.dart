2993fbbe8256c10afa5ab2f060bf825f9d3ebc61
luckuveryx <luckuveryx@icloud.com>
2023-10-14 09:08:02 +0800

    pubspec: add riverpod

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,6 +1,11 @@
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:luckuveryx/app/app.dart';
 import 'package:luckuveryx/bootstrap.dart';
 
 void main() {
-  bootstrap(() => const App());
+  bootstrap(
+    () => const ProviderScope(
+      child: App(),
+    ),
+  );
 }
