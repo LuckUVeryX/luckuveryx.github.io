e7fc5123630106b501ea0e87a586d506f48e1d17
luckuveryx <luckuveryx@icloud.com>
2023-10-14 21:43:20 +0800

    config: usePathUrlStrategy

--- lib/bootstrap.dart
+++ lib/bootstrap.dart
@@ -2,13 +2,12 @@ import 'dart:async';
 import 'dart:developer';
 
 import 'package:flutter/widgets.dart';
+import 'package:flutter_web_plugins/url_strategy.dart';
 
 Future<void> bootstrap(FutureOr<Widget> Function() builder) async {
   FlutterError.onError = (details) {
     log(details.exceptionAsString(), stackTrace: details.stack);
   };
-
-  // Add cross-flavor configuration here
-
+  usePathUrlStrategy();
   runApp(await builder());
 }
