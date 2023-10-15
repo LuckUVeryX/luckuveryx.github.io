7f51d14ebbabdc4822a88018345625448629e467
luckuveryx <luckuveryx@icloud.com>
2023-10-15 13:33:55 +0800

    feat: loading fonts

--- lib/features/root/widgets/pages/root_page.dart
+++ lib/features/root/widgets/pages/root_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:luckuveryx/features/root/root.dart';
+import 'package:luckuveryx/l10n/l10n.dart';
 import 'package:luckuveryx/widgets/widgets.dart';
 
 class RootPage extends HookConsumerWidget {
@@ -16,7 +17,15 @@ class RootPage extends HookConsumerWidget {
     final isLoadingFonts = ref
         .watch(googleFontsPendingProvider.select((value) => value.isLoading));
 
-    if (isLoadingFonts) return const Scaffold();
+    if (isLoadingFonts) {
+      return Scaffold(
+        body: Center(
+          child: LoadingIndicator(
+            label: context.l10n.loadingFonts,
+          ),
+        ),
+      );
+    }
 
     return Scaffold(
       appBar: const RootAppBar(),
