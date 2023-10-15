ac6a4acafb10b7fe8af9a9345856fc9aedc7073a
luckuveryx <luckuveryx@icloud.com>
2023-10-15 18:59:13 +0800

    feat: footer in experiences page

--- lib/features/experiences/widgets/pages/experiences_page.dart
+++ lib/features/experiences/widgets/pages/experiences_page.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:luckuveryx/app/router/router.dart';
 import 'package:luckuveryx/features/experiences/experiences.dart';
 import 'package:luckuveryx/features/responsive/responsive.dart';
+import 'package:luckuveryx/l10n/l10n.dart';
+import 'package:luckuveryx/widgets/molecules/molecules.dart';
 
 class ExperiencesPage extends HookConsumerWidget {
   const ExperiencesPage({super.key});
@@ -9,13 +12,31 @@ class ExperiencesPage extends HookConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final experiences = ref.watch(experiencesProvider);
+    final length = experiences.length;
 
     return ListView.builder(
-      itemCount: experiences.length + 1,
+      itemCount: length + 3,
       padding: ResponsiveLayout.paddingOf(context),
       itemBuilder: (context, index) {
         if (index == 0) {
           return const ExperiencesTitle();
+        }
+
+        if (index == length + 1) {
+          return Align(
+            alignment: Alignment.centerLeft,
+            child: Transform.translate(
+              offset: const Offset(-12, 0),
+              child: NextPageButton(
+                label: context.l10n.viewProjects,
+                onPressed: () => const ProjectsRoute().go(context),
+              ),
+            ),
+          );
+        }
+
+        if (index == length + 2) {
+          return const SocialButtons();
         }
 
         final experience = experiences[index - 1];
