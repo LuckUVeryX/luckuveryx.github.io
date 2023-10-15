8f762bda5669dd6e67a2bbc97df2d9fcd7aa0ae5
luckuveryx <luckuveryx@icloud.com>
2023-10-15 13:03:19 +0800

    feat: animated fade in for material app

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -6,6 +6,7 @@ import 'package:luckuveryx/app/router/router.dart';
 import 'package:luckuveryx/app/theme/theme.dart';
 import 'package:luckuveryx/features/theme_switcher/theme_switcher.dart';
 import 'package:luckuveryx/l10n/l10n.dart';
+import 'package:luckuveryx/widgets/widgets.dart';
 
 class App extends HookConsumerWidget {
   const App({super.key});
@@ -25,14 +26,16 @@ class App extends HookConsumerWidget {
       [notifier],
     );
 
-    return MaterialApp.router(
-      routerConfig: router,
-      theme: AppTheme.light(),
-      darkTheme: AppTheme.dark(),
-      themeMode: ref.watch(themeSwitcherControllerProvider),
-      debugShowCheckedModeBanner: false,
-      supportedLocales: AppLocalizations.supportedLocales,
-      localizationsDelegates: AppLocalizations.localizationsDelegates,
+    return AnimatedFadeIn(
+      child: MaterialApp.router(
+        routerConfig: router,
+        theme: AppTheme.light(),
+        darkTheme: AppTheme.dark(),
+        themeMode: ref.watch(themeSwitcherControllerProvider),
+        debugShowCheckedModeBanner: false,
+        supportedLocales: AppLocalizations.supportedLocales,
+        localizationsDelegates: AppLocalizations.localizationsDelegates,
+      ),
     );
   }
 }
