55d8c80ffc2bc7a4e8282173d1b38ad4ba760317
luckuveryx <luckuveryx@icloud.com>
2023-10-16 14:44:48 +0800

    feat: add side projects

--- lib/features/projects/models/project.dart
+++ lib/features/projects/models/project.dart
@@ -1,6 +1,12 @@
 import 'package:intl/intl.dart';
 import 'package:luckuveryx/features/projects/projects.dart';
 
+const _googlePlay = 'Google Play';
+const _appStore = 'App Store';
+const _gitHub = 'GitHub';
+const _website = 'Website';
+const _testFlight = 'TestFlight';
+
 sealed class Project {
   const Project._({
     required this.title,
@@ -19,14 +25,18 @@ sealed class Project {
   String dateString() {
     final dateFormat = DateFormat.yMMM();
     final start = dateFormat.format(startDate);
-    if (endDate == null) return '$start - Present';
+    if (endDate == null) return start;
     return '$start - ${dateFormat.format(endDate!)}';
   }
 
   static List<Project> get values => [
         Glints(),
+        Portfolio(),
+        Evlve(),
         Osim(),
         Scratchbac(),
+        VjChoirArchives(),
+        BubuDex(),
       ];
 }
 
@@ -42,13 +52,13 @@ class Glints extends Project {
               url: Uri.parse(
                 'https://apps.apple.com/us/app/glints-jobs-chat-hr-career/id1613169954',
               ),
-              label: 'AppStore',
+              label: _appStore,
             ),
             Link(
               url: Uri.parse(
                 'https://play.google.com/store/apps/details?id=com.glints.candidate',
               ),
-              label: 'Google Play',
+              label: _googlePlay,
             ),
           ],
         );
@@ -66,13 +76,13 @@ class Osim extends Project {
               url: Uri.parse(
                 'https://apps.apple.com/sg/app/osim-well-being/id1588337384',
               ),
-              label: 'AppStore',
+              label: _appStore,
             ),
             Link(
               url: Uri.parse(
                 'https://play.google.com/store/apps/details?id=com.osim.oneapp&hl=en&gl=US',
               ),
-              label: 'Google Play',
+              label: _googlePlay,
             ),
           ],
         );
@@ -90,13 +100,103 @@ class Scratchbac extends Project {
               url: Uri.parse(
                 'https://apps.apple.com/sg/app/scratchbac/id1587462661',
               ),
-              label: 'AppStore',
+              label: _appStore,
             ),
             Link(
               url: Uri.parse(
                 'https://play.google.com/store/apps/details?id=com.scratchbac.baladi',
               ),
-              label: 'Google Play',
+              label: _googlePlay,
+            ),
+          ],
+        );
+}
+
+class Portfolio extends Project {
+  Portfolio()
+      : super._(
+          title: 'Portfolio',
+          url: Uri.parse('https://luckuveryx.github.io'),
+          startDate: DateTime(2023, 10),
+          endDate: null,
+          links: [
+            Link(
+              url: Uri.parse(
+                'https://luckuveryx.github.io',
+              ),
+              label: _website,
+            ),
+            Link(
+              url: Uri.parse(
+                'https://github.com/LuckUVeryX/luckuveryx.github.io',
+              ),
+              label: _gitHub,
+            ),
+          ],
+        );
+}
+
+class Evlve extends Project {
+  Evlve()
+      : super._(
+          title: 'Evlve MMA',
+          url: Uri.parse('https://evolve-mma.com/'),
+          startDate: DateTime(2023, 5),
+          endDate: null,
+          links: [
+            Link(
+              url: Uri.parse(
+                'https://github.com/LuckUVeryX/evlve',
+              ),
+              label: _gitHub,
+            ),
+            Link(
+              url: Uri.parse(
+                'https://play.google.com/store/apps/details?id=com.evolve.evlve',
+              ),
+              label: _googlePlay,
+            ),
+            Link(
+              url: Uri.parse(
+                'https://testflight.apple.com/join/eS0JF0F1',
+              ),
+              label: _testFlight,
+            ),
+          ],
+        );
+}
+
+class VjChoirArchives extends Project {
+  VjChoirArchives()
+      : super._(
+          title: 'VJ Choir Archives',
+          url: Uri.parse('https://github.com/LuckUVeryX/vjchoir_archives'),
+          startDate: DateTime(2022, 9),
+          endDate: null,
+          links: [
+            Link(
+              url: Uri.parse(
+                'https://github.com/LuckUVeryX/vjchoir_archives',
+              ),
+              label: _gitHub,
+            ),
+          ],
+        );
+}
+
+class BubuDex extends Project {
+  BubuDex()
+      : super._(
+          title: 'Bubudex',
+          url: Uri.parse('https://github.com/LuckUVeryX/bubudex'),
+          startDate: DateTime(2021, 10),
+          endDate: null,
+          links: [
+            Link(
+              url: Uri.parse(
+                'https://github.com/LuckUVeryX/bubudex',
+              ),
+              label: _gitHub,
             ),
           ],
         );
