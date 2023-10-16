91b47dd419a13f40dab9371494d312276d8707ff
luckuveryx <luckuveryx@icloud.com>
2023-10-16 19:07:26 +0800

    feat: add colors and more layers to background flair

--- lib/features/root/widgets/organisms/root_container.dart
+++ lib/features/root/widgets/organisms/root_container.dart
@@ -5,17 +5,21 @@ import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:luckuveryx/features/root/root.dart';
 import 'package:luckuveryx/utils/theme_extensions.dart';
 
-Iterable<(double height, double value)> _useWaves() {
-  /// (height, speed, offset)
-  const curves = [
-    (160.0, 0.5, 0.0),
-    (200.0, 1.0, pi),
-    (240.0, 1.5, pi * 2),
+Iterable<(double height, double value, Color color)> _useWaves() {
+  final colorScheme = useContext().colorScheme;
+
+  /// (height, speed, offset, color)
+  final curves = [
+    (80.0, 0.5, -pi * 2, colorScheme.primary),
+    (120.0, 1.0, -pi, colorScheme.primaryContainer),
+    (160.0, 1.5, 0.0, colorScheme.secondary),
+    (200.0, 1.0, pi, colorScheme.secondaryContainer),
+    (240.0, 0.5, pi * 2, colorScheme.tertiary),
   ];
 
   return curves.map(
     (e) {
-      final (height, speed, offset) = e;
+      final (height, speed, offset, color) = e;
 
       final controller = useAnimationController(
         duration: Duration(milliseconds: 5000 ~/ speed),
@@ -28,9 +32,17 @@ Iterable<(double height, double value)> _useWaves() {
         [],
       );
 
-      final value =
-          useAnimation(controller.drive(Tween(begin: 0, end: 2 * pi))) + offset;
-      return (height, value);
+      final value = useAnimation(
+            controller.drive(
+              Tween(begin: 0, end: 2 * pi).chain(
+                CurveTween(
+                  curve: Curves.easeInOutSine,
+                ),
+              ),
+            ),
+          ) +
+          offset;
+      return (height, value, color.withOpacity(0.1));
     },
   );
 }
@@ -57,7 +69,7 @@ class RootContainer extends HookWidget {
       margin: const EdgeInsets.all(28).copyWith(left: 0),
       child: Stack(
         children: [
-          for (final (height, value) in waves)
+          for (final (height, value, color) in waves)
             Positioned(
               left: 0,
               right: 0,
@@ -68,7 +80,7 @@ class RootContainer extends HookWidget {
                 child: CustomPaint(
                   painter: CurvePainter(
                     value: value,
-                    color: context.colorScheme.onBackground.withOpacity(0.1),
+                    color: color,
                   ),
                 ),
               ),
