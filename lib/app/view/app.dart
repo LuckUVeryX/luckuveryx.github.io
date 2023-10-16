10fdde242c0c3020b80d5cfc045b629720f20a96
luckuveryx <luckuveryx@icloud.com>
2023-10-16 15:19:50 +0800

    feat: disable scrollbars

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:go_router/go_router.dart';
@@ -37,7 +38,21 @@ class App extends HookConsumerWidget {
         debugShowCheckedModeBanner: false,
         supportedLocales: AppLocalizations.supportedLocales,
         localizationsDelegates: AppLocalizations.localizationsDelegates,
+        scrollBehavior: const NoThumbScrollBehavior().copyWith(
+          scrollbars: false,
+        ),
       ),
     );
   }
+}
+
+class NoThumbScrollBehavior extends ScrollBehavior {
+  const NoThumbScrollBehavior();
+  @override
+  Set<PointerDeviceKind> get dragDevices => {
+        PointerDeviceKind.touch,
+        PointerDeviceKind.mouse,
+        PointerDeviceKind.stylus,
+        PointerDeviceKind.trackpad,
+      };
 }
