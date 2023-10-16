0fa5d04f706ec3ecc8b6760e348d0b47bad32cc4
luckuveryx <luckuveryx@icloud.com>
2023-10-16 15:15:58 +0800

    fix: project header not having correct background color

--- lib/features/projects/widgets/pages/projects_page.dart
+++ lib/features/projects/widgets/pages/projects_page.dart
@@ -1,9 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:luckuveryx/features/projects/projects.dart';
-import 'package:luckuveryx/l10n/l10n.dart';
-import 'package:luckuveryx/utils/theme_extensions.dart';
-import 'package:luckuveryx/widgets/widgets.dart';
 
 class ProjectsPage extends StatelessWidget {
   const ProjectsPage({super.key});
@@ -18,23 +14,9 @@ class ProjectsPage extends StatelessWidget {
         padding: const EdgeInsets.fromLTRB(0, 116, 8, 120),
         itemBuilder: (context, index) {
           if (index == 0) {
-            return Padding(
-              padding: const EdgeInsets.only(bottom: 32, right: 16),
-              child: Row(
-                mainAxisSize: MainAxisSize.min,
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  const FaIcon(
-                    FontAwesomeIcons.solidSquare,
-                    size: 16,
-                  ),
-                  Spacing.sp4,
-                  Text(
-                    context.l10n.projects,
-                    style: context.textTheme.titleSmall,
-                  ),
-                ],
-              ),
+            return const Padding(
+              padding: EdgeInsets.only(bottom: 32, right: 16),
+              child: ProjectHeader(),
             );
           }
           final project = projects[index - 1];
