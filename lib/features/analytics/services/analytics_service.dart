f6dbef70b614927be30f076f54d6cedd62eaf05e
luckuveryx <luckuveryx@icloud.com>
2023-10-21 14:53:49 +0800

    feat: analytics event tracking

---
+++ lib/features/analytics/services/analytics_service.dart
@@ -0,0 +1,36 @@
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:luckuveryx/features/analytics/analytics.dart';
+import 'package:posthog_flutter/posthog_flutter.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'analytics_service.g.dart';
+
+@riverpod
+AnalyticsService _analyticsService(_AnalyticsServiceRef ref) {
+  return AnalyticsService._(Posthog());
+}
+
+class AnalyticsService {
+  const AnalyticsService._(this._posthog);
+
+  final Posthog _posthog;
+
+  void capture(AnalyticsEvent event) {
+    _posthog.capture(
+      eventName: event.eventName,
+      properties: event.properties,
+    );
+  }
+}
+
+extension AnalyticsRefX on Ref {
+  void capture(AnalyticsEvent event) {
+    read(_analyticsServiceProvider).capture(event);
+  }
+}
+
+extension AnalyticsWidgetRefX on WidgetRef {
+  void capture(AnalyticsEvent event) {
+    read(_analyticsServiceProvider).capture(event);
+  }
+}
