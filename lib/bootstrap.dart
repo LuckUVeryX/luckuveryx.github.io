36f3398e5b006c25fe2052cd2acb22ded21534a5
luckuveryx <luckuveryx@icloud.com>
2024-05-15 21:00:34 +0800

    config: move posthog key to index.html

--- lib/bootstrap.dart
+++ lib/bootstrap.dart
@@ -1,26 +1,12 @@
-// ignore_for_file: avoid_web_libraries_in_flutter
-
 import 'dart:async';
 import 'dart:developer';
-import 'dart:html' as html;
-import 'dart:js_interop' as web;
 
-import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart';
-import 'package:luckuveryx/app/api/api.dart';
 
 Future<void> bootstrap(FutureOr<Widget> Function() builder) async {
   FlutterError.onError = (details) {
     log(details.exceptionAsString(), stackTrace: details.stack);
   };
 
-  if (kIsWeb) {
-    postHogApiKey = ApiKeys.postHog;
-    html.document.dispatchEvent(html.CustomEvent('keys_loaded'));
-  }
-
   runApp(await builder());
 }
-
-@web.JS('POST_HOG_API_KEY')
-external set postHogApiKey(String value);
