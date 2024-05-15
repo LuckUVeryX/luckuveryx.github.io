5c31b66242c2c0c17a1ebbfe411b89f96a7c5d8f
luckuveryx <luckuveryx@icloud.com>
2024-05-15 20:45:08 +0800

    feat: use web deps

--- lib/bootstrap.dart
+++ lib/bootstrap.dart
@@ -2,12 +2,12 @@
 
 import 'dart:async';
 import 'dart:developer';
-import 'dart:html' as html;
-import 'dart:js' as js;
+import 'dart:js_interop' as web;
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart';
 import 'package:luckuveryx/app/api/api.dart';
+import 'package:web/web.dart' as web;
 
 Future<void> bootstrap(FutureOr<Widget> Function() builder) async {
   FlutterError.onError = (details) {
@@ -15,9 +15,12 @@ Future<void> bootstrap(FutureOr<Widget> Function() builder) async {
   };
 
   if (kIsWeb) {
-    js.context['POST_HOG_API_KEY'] = ApiKeys.postHog;
-    html.document.dispatchEvent(html.CustomEvent('keys_loaded'));
+    postHogApiKey = ApiKeys.postHog;
+    web.document.dispatchEvent(web.CustomEvent('keys_loaded'));
   }
 
   runApp(await builder());
 }
+
+@web.JS('POST_HOG_API_KEY')
+external set postHogApiKey(String value);
