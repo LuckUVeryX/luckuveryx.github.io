0251f1adcc6cb3587314499166c847d92133e50f
luckuveryx <luckuveryx@icloud.com>
2024-05-15 20:48:41 +0800

    fix: revert to using html

--- lib/bootstrap.dart
+++ lib/bootstrap.dart
@@ -2,12 +2,12 @@
 
 import 'dart:async';
 import 'dart:developer';
+import 'dart:html' as html;
 import 'dart:js_interop' as web;
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart';
 import 'package:luckuveryx/app/api/api.dart';
-import 'package:web/web.dart' as web;
 
 Future<void> bootstrap(FutureOr<Widget> Function() builder) async {
   FlutterError.onError = (details) {
@@ -16,7 +16,7 @@ Future<void> bootstrap(FutureOr<Widget> Function() builder) async {
 
   if (kIsWeb) {
     postHogApiKey = ApiKeys.postHog;
-    web.document.dispatchEvent(web.CustomEvent('keys_loaded'));
+    html.document.dispatchEvent(html.CustomEvent('keys_loaded'));
   }
 
   runApp(await builder());
